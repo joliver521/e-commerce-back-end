2858380127e9bda9db2b810a9ab72b3c1e47fdc8
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-08-03 19:40:19 -0400

    Category.js model created

--- models/Category.js
+++ models/Category.js
@@ -5,16 +5,27 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
+    {
+        // define columns: id
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true,
+        },
+        // defining column: category_name
+        category_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'category',
+    }
 );
 
 module.exports = Category;
