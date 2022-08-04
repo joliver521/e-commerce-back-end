a2f12f1bbf3345e5f93a8842e4c9172eecad1521
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-08-03 20:06:25 -0400

    ProductTag.js models created

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -5,16 +5,40 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
+    {
+        // define columns
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true,
+        },
+        // defining column: product_id
+        product_id: {
+            type: DataTypes.INTEGER,
+            // referencing the product model's ids
+            references: {
+                model: 'product',
+                key: 'id',
+            },
+        },
+        // defining column: tag_id
+        tag_id: {
+            type: DataTypes.INTEGER,
+            // referencing the tag model's ids
+            references: {
+                model: 'tag',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product_tag',
+    }
 );
 
 module.exports = ProductTag;
