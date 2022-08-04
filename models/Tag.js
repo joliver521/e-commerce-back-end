b9662818ec65b671783c3b9cb7a582f4fe340ada
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-08-03 20:47:12 -0400

    Tag.js and ProductTag.js updated

--- models/Tag.js
+++ models/Tag.js
@@ -5,16 +5,26 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'tag',
-  }
+    {
+        // define columns: id
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true,
+        },
+        // defining column: tag_name
+        tag_name: {
+            type: DataTypes.STRING,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'tag',
+    }
 );
 
 module.exports = Tag;
